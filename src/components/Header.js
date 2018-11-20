0a6e065a21f72317b26cf5f4637a2bc32321a2f2
Leandro Cunha <leandroscunha@gmail.com>
2018-11-19 22:01:53 -0200

    improvement(Header): apply layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderStyled = styled.header`
+  align-items: center;
+  background-color: #00adff;
+  box-shadow: rgba(0, 0, 0, 0.08) 1px 1px 4px 0.5px;
+  display: flex;
+  padding: 15px;
+`;
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const Avatar = styled.img`
+  border: #008ee0 2px solid;
+  border-radius: 4px;
+  margin: 0 12px 0 0;
+`;
 
 const Header = ({ player }) => (
-  <header>
-    <h1>{`Player ${player}`}</h1>
-  </header>
+  <HeaderStyled>
+    <Avatar
+      alt={`player ${player}`}
+      src={`https://api.adorable.io/avatars/20/${player}.png`}
+    />
+    <Title>{`Player ${player}`}</Title>
+  </HeaderStyled>
 );
 
 export default Header;
